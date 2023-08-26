80d042f3850a29cea9b4b59a9f85db9be7d2eafa
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-26 16:57:28 +0500

    create sign up api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.router.js";
+import authRouter from "./routes/auth.router.js";
 const app = express();
 dotenv.config();
 
@@ -13,8 +14,12 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
-app.use("/api/user", userRouter);
