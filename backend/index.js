1b5973b6d6ad0f47dce27aa3501bf1f2686e4be8
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-28 16:34:44 +0500

    add pre production changes

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.router.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 const app = express();
 dotenv.config();
 
+const __dirname = path.resolve();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -16,9 +18,13 @@ mongoose
     console.log(err);
   });
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
